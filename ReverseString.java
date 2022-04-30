ce362379fd330dbb76e67dd8accb970f187b56c5
AllKeng <87668303+AllKeng@users.noreply.github.com>
2022-04-29 20:48:04 -0700

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+    public static String reverse(String str) {
+        String result = "";
+        String letter = "";
+        for(int i = 0; i < str.length(); i++) {
+            letter = str.substring(i,i+1);
+            result += letter; 
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(reverse(args[0]));
+    }
+}
